9cbee8ee64609c15b486a7256a71a336a9f82def
pornthip08win06-web <pornthip08win06@gmail.com>
2026-08-25 09:09:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
+   document.getElementByld('year').innerHTML=new Date().getFullYear
    console.log('Portfolio ของ pornthip พร้อมแล้ว!');
 }};
